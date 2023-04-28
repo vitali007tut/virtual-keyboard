572b5d54eec3427ea09a3bfcda2af35089d427ab
vitali007tut <vitali007@tut.by>
2023-04-28 09:17:04 +0300

    feat: add style tittle and container

--- js/app.js
+++ js/app.js
@@ -2,15 +2,29 @@ import { createLiter } from './addComponent.js'
 
 // * add style in html
 const style = window.document.createElement('link')
-style.href = "style.css"
+style.href = "./css/style.css"
 style.rel = "stylesheet"
 window.document.getElementsByTagName('head')[0].appendChild(style)
 
 const body = document.querySelector('body')
 
-const textArea = document.createElement('textarea')
-textArea.style = 'display: block;width: 80vw;height: 20vh;margin: 0px auto;'
-document.body.append(textArea)
+ // * add container
+const container = document.createElement('div')
+container.classList.add('container')
+document.body.appendChild(container)
 
+// * add title
+const title = document.createElement('p')
+title.classList.add('title')
+title.innerHTML = 'Виртуальная клавиатура'
+container.appendChild(title)
+
+// * add textArea
+const textArea = document.createElement('textarea')
+textArea.classList.add('textarea')
+container.appendChild(textArea)
+
+
+// test
 const component = createLiter('R') // example
 body.appendChild(component)
